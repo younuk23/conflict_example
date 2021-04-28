940a0ceaaaaaddf7e45a740e51b177fbcb341bc2
Yeonuk Hwang <younuk23@gmail.com>
2021-04-28 15:06:29 +0900

    Add: Initial Setting

---
+++ src/pages/sunghoon/main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MainSunghoon extends React.Component {
+  render() {
+    return <h1>Sunghoon Main</h1>;
+  }
+}
+
+export default MainSunghoon;
---
+++ src/pages/yeonuk/main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MainYeonuk extends React.Component {
+  render() {
+    return <h1>Yeonuk Main</h1>;
+  }
+}
+
+export default MainYeonuk;
