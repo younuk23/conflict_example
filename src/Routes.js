91d442628680288fd8ee414aaeda2e08c3d9529a
Yeonuk Hwang <54208214+younuk23@users.noreply.github.com>
2021-06-30 17:14:17 +0900

    Revert "[김성훈] Add: 로그인 페이지 완성"

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginSunghoon from "./pages/sunghoon/login/Login";
+
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/login-sunghoon" component={LoginSunghoon} />
-        </Switch>
+        <Switch></Switch>
       </Router>
     );
   }
